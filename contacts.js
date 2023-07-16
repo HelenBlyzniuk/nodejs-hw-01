11bda025ae4a7800f99da8e7583970eca6fb64d5
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-07-16 12:05:55 +0300

    try-catch

--- contacts.js
+++ contacts.js
@@ -6,33 +6,55 @@ import { nanoid } from "nanoid";
 const contactsPath = path.resolve("db", "contacts.json");
 
 export async function listContacts() {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error.message)
+  }
+
 }
 
 export async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const result = contacts.find((item) => item.id === contactId);
-  return result || null;
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === contactId);
+    return result || null;
+    
+  } catch (error) {
+    console.log(error.message)
+  }
+ 
 }
 
 export async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
-  if (index === -1) {
-    return null;
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = await contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.log(error.message)
   }
-  const [result] = await contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
+
 }
 
 export async function addContact(name, email, phone) {
-  const contacts = await listContacts();
+  try {
+    const contacts = await listContacts();
   const newContact = { id: nanoid(), name, email, phone };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
+  } catch (error) {
+    console.log(error.message)
+  }
+  
+ 
 }
 
 
