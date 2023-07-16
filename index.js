be577efda039e990c050b6fb4dba20f06e925f5e
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-07-16 11:57:13 +0300

    commander

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-import contactServices from './contacts.js';
+import { program } from "commander";
+import contactServices from "./contacts.js";
 
+
+program
+  .option("-a, --action <type>")
+  .option("-i,--id <type>")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+  program.parse(process.argv);
+  
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
-    switch (action) {
-      case 'list':
-        const allContacts=await contactServices.listContacts();
-        return console.log(allContacts);
-       
-  
-      case 'get':
-        const contact =await contactServices.getContactById(id);
-        return console.log(contact)
-        break;
-  
-      case 'add':
-        const newContact=await contactServices.addContact(name, email, phone);
-        return console.log(newContact);
-       
-  
-      case 'remove':
-        const deletedContact=await contactServices.removeContact(id)
-        return console.log(deletedContact);
-  
-      default:
-        console.warn('\x1B[31m Unknown action type!');
-    }
+  switch (action) {
+    case "list":
+      const allContacts = await contactServices.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const contact = await contactServices.getContactById(id);
+      return console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contactServices.addContact(name, email, phone);
+      return console.log(newContact);
+
+    case "remove":
+      const deletedContact = await contactServices.removeContact(id);
+      return console.log(deletedContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
-  
-  // invokeAction({action:'list'})
-  // invokeAction({action:'get',id:'e6ywwRe4jcqxXfCZOj_1e'})
-  //  invokeAction({action:'add', name:"Lena",email:"helen@gmail.com",phone:"4432156"})
-  //  invokeAction({action:'remove',id:'e6ywwRe4jcqxXfCZOj_1e'})
+}
+
+invokeAction(argv);
+// invokeAction({action:'list'})
+// invokeAction({action:'get',id:'e6ywwRe4jcqxXfCZOj_1e'})
+//  invokeAction({action:'add', name:"Lena",email:"helen@gmail.com",phone:"4432156"})
+//  invokeAction({action:'remove',id:'e6ywwRe4jcqxXfCZOj_1e'})
